205420b4e098568957a42b1043679bbb45b2f238
Juok Lee <juoklee@mirabellsoft.com>
2024-08-06 17:46:12 +0900

    [level 1] Title: 최소직사각형, Time: 1.84 ms, Memory: 83.8 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -1,18 +1,11 @@
 class Solution {
     public int solution(int[][] sizes) {
-        int[] max = {0,0};
-        
-        for(int i=0; i<sizes.length; i++) {
-            if(sizes[i][0]<sizes[i][1]){
-                int tmp = sizes[i][0];
-                sizes[i][0] = sizes[i][1];
-                sizes[i][1] = tmp;
-            }
-            
-            if(max[0]<sizes[i][0]) { max[0] = sizes[i][0]; }
-            if(max[1]<sizes[i][1]) { max[1] = sizes[i][1]; }
+       int length = 0, height = 0;
+        for (int[] card : sizes) {
+            length = Math.max(length, Math.max(card[0], card[1]));
+            height = Math.max(height, Math.min(card[0], card[1]));
         }
-        
-        return max[0]*max[1];
+        int answer = length * height;
+        return answer;
     }
 }
